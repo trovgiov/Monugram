4ba1a174d4b7a7ee2a37d3f2a5d47dd92456f276
Enifix <eneamarinelli@gmail.com>
2017-04-21 11:05:40 +0200

    Check User email

--- src/it/uniclam/db/RegisterDAO.java
+++ src/it/uniclam/db/RegisterDAO.java
@@ -4,43 +4,78 @@ import it.uniclam.action.RegisterAction;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * Created by GiovanniTrovini on 20/04/17.
  */
 public class RegisterDAO {
 
-    public static int save(RegisterAction r) {
 
-        int status = 0;
+    public static boolean checkUser(RegisterAction r)
+    {
+        /*
+            CONTROLLO SE L'EMAIL E' GIA' PRESENTE NEL DB
+         */
+        boolean end = false;
 
         try {
+            Connection con2 = DBUtility.getDBConnection();
 
-            //Connessione al db
-            Connection con = DBUtility.getDBConnection();
-            PreparedStatement stmt = con
-                    .prepareStatement("insert into User (nome,cognome,email,password, point) values(?,?,?,?,3)");
-
-            stmt.setString(1,r.getNome());
-            stmt.setString(2,r.getCognome());
-            stmt.setString(3,r.getEmail());
-            stmt.setString(4,r.getPassword());
+            String tst = null;
+            String sql = "SELECT email from User";
 
 
+            java.sql.Statement stm2 = con2.createStatement();
 
+            ResultSet rs2 = stm2.executeQuery(sql);
 
+            while (rs2.next()) {
+                tst = rs2.getString("email");
+                if (r.getEmail().equals(tst)) {
+                    end = true;
+                }
+            }
 
-
-            status=stmt.executeUpdate(); // execute query
+            stm2.close();
+            rs2.close();
+            con2.close();
 
         } catch (Exception e) {
             System.out.println(e);
         }
-        return status;
+        return end;
     }
 
+    public static int save(RegisterAction r)
+    {
 
+        int status = 0;
+        boolean check = checkUser(r);
+        //System.out.println(".............."+check);
 
+        if(check == false)
+        {
+            // Inserimento Utente solo se check uguale a false
+            try {
 
+                //Connessione al db
+                Connection con = DBUtility.getDBConnection();
+                PreparedStatement stmt = con
+                        .prepareStatement("insert into User (nome,cognome,email,password, point) values(?,?,?,?,3)");
 
+                stmt.setString(1, r.getNome());
+                stmt.setString(2, r.getCognome());
+                stmt.setString(3, r.getEmail());
+                stmt.setString(4, r.getPassword());
+
+                status = stmt.executeUpdate(); // execute query
+
+                stmt.close();
+                con.close();
+            } catch (Exception e) {
+            }
+        }
+        return status;
+    }
 }
